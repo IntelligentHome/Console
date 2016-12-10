87c421458f8d88e65e655136a2df6106087871a2
Mariusz Sikora <msikora87@gmail.com>
2016-12-10 11:29:37 +0100

    Initial project setup

---
+++ Console.cpp
@@ -0,0 +1,6 @@
+
+#include "Console.h"
+
+namespace console {
+
+} /* namespace console */
---
+++ Console.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "ITransport.h"
+
+namespace console {
+
+
+} /* namespace console */
